6ee7f6337223a6190d623e4a84ab6000edd3c216
Paul Iogna <paul.iogna@gmail.com>
2015-12-28 17:20:43 -0500

    Calculator finished

---
+++ calculator.rb
@@ -0,0 +1,19 @@
+class Calculator
+  def add(a, b)
+    # your code here
+    return a + b
+  end
+
+  def subtract(a, b)
+    # your code here
+    return a - b
+  end
+
+  def multiply(a, b)
+    return a * b
+  end
+
+  def divide(a, b)
+    return a / b
+  end
+end
